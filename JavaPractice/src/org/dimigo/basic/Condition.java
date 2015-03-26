427c3f2b29bae1acbbca8670f289445b8ee67ad5
Owner <Owner@NT450R5E-K2WS>
2015-03-26 11:38:58 +0900

    통행료계산기수정

--- JavaPractice/src/org/dimigo/basic/Condition.java
+++ JavaPractice/src/org/dimigo/basic/Condition.java
@@ -7,34 +7,16 @@ public class Condition {
 		int distance = 10;
 		int money=(distance-10)/10;
 		System.out.println("<<고속도로통행료계산>>");
-		if(distance==10){
-			System.out.println("거리 : 10km");
-		    if(type==1){
-			    System.out.println("차량 : 고속버스");
-			    System.out.println("통행료 : 850원");
-		    }else if(type ==2){
-			    System.out.println("차량 : 경차");
-			    System.out.println("통행료 : 300원");
-		    }else if(type == 3){
-			    System.out.println("차량 : 그외");
-			    System.out.println("통행료 : 600원");
-		    }
-		}
-		if(distance>10){
-			System.out.println("거리 : "+distance+"km");
-			if(type==1){
-				System.out.println("차량 : 고속버스");
-				System.out.println("통행료 : "+(850+300*money)+"원");
-			}
-			if(type==2){
-				System.out.println("차량 : 경차");
-				System.out.println("통행료 : "+(300+200*money)+"원");
-			}
-			if(type==3){
-				System.out.println("차량 : 그외");
-				System.out.println("통행료 : "+(600+200*money)+"원");
+		System.out.println("거리 : "+distance+"km");
+		if(type==1){
+			System.out.println("차량 : 고속버스");
+			System.out.println("통행료 : "+(850+300*money)+"원");
+		}if(type==2){
+			System.out.println("차량 : 경차");
+			System.out.println("통행료 : "+(300+200*money)+"원");
+		}if(type==3){
+			System.out.println("차량 : 그외");
+			System.out.println("통행료 : "+(600+200*money)+"원");
 			}
 		}
 	}
-
-}
