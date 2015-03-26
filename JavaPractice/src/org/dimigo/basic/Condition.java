79c953eddae01abcec3cfdfcc3da0043db7bf286
Owner <Owner@NT450R5E-K2WS>
2015-03-26 11:53:08 +0900

    통행료!완성본!

--- JavaPractice/src/org/dimigo/basic/Condition.java
+++ JavaPractice/src/org/dimigo/basic/Condition.java
@@ -5,18 +5,18 @@ public class Condition {
 	public static void main(String[] args) {
 		int type = 1;
 		int distance = 10;
-		int money=(distance-10)/10;
+		int money=(distance-1)/10;
 		System.out.println("<<고속도로통행료계산>>");
 		System.out.println("거리 : "+distance+"km");
 		if(type==1){
 			System.out.println("차량 : 고속버스");
 			System.out.println("통행료 : "+(850+300*money)+"원");
 		}if(type==2){
-			System.out.println("차량 : 경차");
+		    System.out.println("차량 : 경차");
 			System.out.println("통행료 : "+(300+200*money)+"원");
 		}if(type==3){
 			System.out.println("차량 : 그외");
 			System.out.println("통행료 : "+(600+200*money)+"원");
-			}
 		}
-	} 
+	}
+	}
