93a3486b15aa5b5d02d8bd1c45aa5800eb103dda
Owner <Owner@NT450R5E-K2WS>
2015-09-01 10:17:39 +0900

    추상클래스실습과제1

---
+++ JavaPractice/src/org/dimigo/abstractclass/SmartPhoneTest.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package org.dimigo.abstractclass;
+
+/**
+ * <pre>
+ *&{package_name}
+ *    |_SmartPhoneTest
+ *
+ *1. 개요 : 
+ *2. 작성일 : 2015. 8. 31.
+ *</pre>
+ * @author   : 김정수
+ * @version  : 1.0
+ */
+public class SmartPhoneTest {
+
+	/**
+	 * @param args
+	 */
+  public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	SmartPhone[] phonelist = {new iPhone("iPhone 6","애플",700000),new Galaxy("GalaxyS6","삼성",650000)};
+		for(SmartPhone phone : phonelist){
+			System.out.println(phone.toString());
+			phone.turnOn();
+			phone.pay();
+			phone.useSpecialFunction();
+			phone.turnOff();
+		}
+	}
+}
---
+++ JavaPractice/src/org/dimigo/abstractclass/iPhone.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package org.dimigo.abstractclass;
+
+/**
+ * <pre>
+ *&{package_name}
+ *    |_iPhone
+ *
+ *1. 개요 : 
+ *2. 작성일 : 2015. 8. 25.
+ *</pre>
+ * @author   : 김정수
+ * @version  : 1.0
+ */
+public class iPhone extends SmartPhone {
+	public iPhone(){	
+		
+	}
+			
+	public iPhone(String model, String company, int price){
+		super(model,company,price);
+	}
+			
+	public void pay(){
+		System.out.println("애플 페이로 결제합니다.");
+	}
+			
+	public void useAirDrop(){
+		System.out.println("AirDrop 기능을 사용합니다.");
+	}
+}
